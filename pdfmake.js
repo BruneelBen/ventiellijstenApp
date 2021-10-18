a0ae78eae77641deb004dbbfe3749d5982472e45
BruneelBen <benbruneel@hotmail.com>
2021-10-18 21:56:06 +0200

    auto multi page build and not to moch afd

--- pdfmake.js
+++ pdfmake.js
@@ -14,101 +14,92 @@ var printer = new PdfPrinter(fonts);
 var fs = require('fs');
 const { data } = require('jquery');
 
-var pageBuilding = {
-    name: 'Stal 4',
-    fontSize: 8.5,
-    LafUp: true,
-    RafUp: true,
-    VentPerAfd: 10,
-    LStartVent: 1,
-    LStartAfd: 40,
-    LAantalAfd: 5,
-    RStartVent: 51,
-    RStartAfd: 45,
-    RAantalAfd: 5
-};
-var pageBuilding2 = {
-    name: 'Stal 4',
-    fontSize: 8.5,
-    LafUp: false,
-    RafUp: false,
-    VentPerAfd: 10,
-    LStartVent: 101,
-    LStartAfd: 39,
-    LAantalAfd: 5,
-    RStartVent: 151,
-    RStartAfd: 34,
-    RAantalAfd: 4
-};
-var pageBuilding3 = {
-    name: 'Stal 2',
-    fontSize: 10,
-    LafUp: true,
-    RafUp: false,
-    VentPerAfd: 4,
-    LStartVent: 1,
-    LStartAfd: 12,
-    LAantalAfd: 6,
-    RStartVent: 25,
-    RStartAfd: 11,
-    RAantalAfd: 6
-};
-var pageBuilding4 = {
-    name: 'Stal 3',
-    fontSize: 10,
-    LafUp: true,
-    RafUp: false,
-    VentPerAfd: 5,
-    LStartVent: 1,
-    LStartAfd: 24,
-    LAantalAfd: 6,
-    RStartVent: 31,
-    RStartAfd: 23,
-    RAantalAfd: 6
-};
-var pageBuilding5 = {
-    name: 'Stal 5',
-    fontSize: 10,
-    LafUp: true,
-    RafUp: true,
-    VentPerAfd: 8,
-    LStartVent: 1,
-    LStartAfd: 50,
-    LAantalAfd: 3,
-    RStartVent: 25,
-    RStartAfd: 53,
-    RAantalAfd: 3
-};
-var pageBuilding6 = {
-    name: 'Stal 5',
-    fontSize: 10,
-    LafUp: false,
-    RafUp: false,
-    VentPerAfd: 8,
-    LStartVent: 49,
-    LStartAfd: 61,
-    LAantalAfd: 3,
-    RStartVent: 73,
-    RStartAfd: 58,
-    RAantalAfd: 3
-};
+var pageBuilding = [{
+        name: 'Stal 4',
+        fontSize: 8.5,
+        LafUp: true,
+        RafUp: true,
+        VentPerAfd: 10,
+        LStartVent: 1,
+        LStartAfd: 40,
+        LAantalAfd: 5,
+        RStartVent: 51,
+        RStartAfd: 45,
+        RAantalAfd: 5
+    },
+        {
+        name: 'Stal 4',
+        fontSize: 8.5,
+        LafUp: false,
+        RafUp: false,
+        VentPerAfd: 10,
+        LStartVent: 101,
+        LStartAfd: 39,
+        LAantalAfd: 5,
+        RStartVent: 151,
+        RStartAfd: 34,
+        RAantalAfd: 4
+    },
+    {
+        name: 'Stal 2',
+        fontSize: 10,
+        LafUp: true,
+        RafUp: false,
+        VentPerAfd: 4,
+        LStartVent: 1,
+        LStartAfd: 12,
+        LAantalAfd: 6,
+        RStartVent: 25,
+        RStartAfd: 11,
+        RAantalAfd: 6
+    },
+    {
+        name: 'Stal 3',
+        fontSize: 10,
+        LafUp: true,
+        RafUp: false,
+        VentPerAfd: 5,
+        LStartVent: 1,
+        LStartAfd: 24,
+        LAantalAfd: 6,
+        RStartVent: 31,
+        RStartAfd: 23,
+        RAantalAfd: 6
+    },
+    {
+        name: 'Stal 5',
+        fontSize: 10,
+        LafUp: true,
+        RafUp: true,
+        VentPerAfd: 8,
+        LStartVent: 1,
+        LStartAfd: 50,
+        LAantalAfd: 3,
+        RStartVent: 25,
+        RStartAfd: 53,
+        RAantalAfd: 3
+    },
+    {
+        name: 'Stal 5',
+        fontSize: 10,
+        LafUp: false,
+        RafUp: false,
+        VentPerAfd: 8,
+        LStartVent: 49,
+        LStartAfd: 61,
+        LAantalAfd: 3,
+        RStartVent: 73,
+        RStartAfd: 58,
+        RAantalAfd: 3
+    }];
 
 async function createPDF(inputData) {
+    // start building the page
     var docDefenition = {
         pageSize: 'A4',
         pageOrientation: 'landscape',
         pageMargins: [10, 10, 10, 10],
-        styles: {
-            table1: {
-                fontSize: 8.5,
-                bold: false
-            },
-            table2: {
-                fontSize: 16,
-                bold: true,
-                margin: [0, 10, 0, 5]
-            }
-        },
+        styles: {},
         defaultStyle: {
             fontSize: 10
         },
@@ -118,133 +109,55 @@ async function createPDF(inputData) {
 
     };
 
-    var table = {
-        style: 'table1',
-        table: {
-            headerRows: 1,
-            widths: [20, 20, '*', '*', '*', '*', '*', '*', '*', 20, 20, '*', '*', '*', '*', '*', '*', '*'],
+    // add lineheigts to document
+    pageBuilding.forEach((page, index) => {
+        docDefenition.styles["page"+index] = {fontSize: page.fontSize, bold: false};
+    });
 
-            body: []
-        }, layout: {
-				hLineWidth: function (i, node) {
-					return (i === 0 || i == 1 || i === node.table.body.length || (i - 1) % 10 == 0) ? 2 : 0.1;
-                    //return 0.1;
-				},
-				vLineWidth: function (i, node) {
-					return (i === 0 || i == 1 || i == 2 || i == 9 || i == 10 || i == 11 || i === node.table.widths.length) ? 2 : 0.1;
-                    return 0.1;
-				},
-				hLineColor: function (i, node) {
-					// return (i === 0 || i === node.table.body.length) ? 'black' : 'gray';
-                    return 'black';
-				},
-				vLineColor: function (i, node) {
-					// return (i === 0 || i === node.table.widths.length) ? 'black' : 'gray';
-                    return 'black';
-				},
-				// hLineStyle: function (i, node) { return {dash: { length: 10, space: 4 }}; },
-				// vLineStyle: function (i, node) { return {dash: { length: 10, space: 4 }}; },
-				paddingLeft: function(i, node) { return 5; },
-				paddingRight: function(i, node) { return 5; },
-				paddingTop: function(i, node) { return 0.1; },
-				paddingBottom: function(i, node) { return 0.1; },
-				// fillColor: function (rowIndex, node, columnIndex) { return null; }
-			}
-    };
-    buildPage(table, pageBuilding, inputData);
-    docDefenition.content.push({text: 'Weeknummer: ', alignment: 'center', style: 'subheader'});
-    docDefenition.content.push(table);
+    // each page building
+    pageBuilding.forEach((page, index) => {
+        // table preparing
+        var table = {
+            style: "page"+index,
+            table: {
+                headerRows: 1,
+                widths: [20, 20, '*', '*', '*', '*', '*', '*', '*', 20, 20, '*', '*', '*', '*', '*', '*', '*'],
 
-    var table = {
-        table: {
-            headerRows: 1,
-            widths: [20, 20, '*', '*', '*', '*', '*', '*', '*', 20, 20, '*', '*', '*', '*', '*', '*', '*'],
-
-            body: []
-        }, layout: {
-				hLineWidth: function (i, node) {
-					// return (i === 0 || i === node.table.body.length) ? 2 : 1;
-                    return 0.1;
-				},
-				vLineWidth: function (i, node) {
-					// return (i === 0 || i === node.table.widths.length) ? 2 : 1;
-                    return 0.1;
-				},
-				hLineColor: function (i, node) {
-					// return (i === 0 || i === node.table.body.length) ? 'black' : 'gray';
-                    return 'black';
-				},
-				vLineColor: function (i, node) {
-					// return (i === 0 || i === node.table.widths.length) ? 'black' : 'gray';
-                    return 'black';
-				},
-				// hLineStyle: function (i, node) { return {dash: { length: 10, space: 4 }}; },
-				// vLineStyle: function (i, node) { return {dash: { length: 10, space: 4 }}; },
-				paddingLeft: function(i, node) { return 5; },
-				paddingRight: function(i, node) { return 5; },
-				paddingTop: function(i, node) { return 0.2; },
-				paddingBottom: function(i, node) { return 0.2; },
-				// fillColor: function (rowIndex, node, columnIndex) { return null; }
-			}
-    };
-    buildPage(table, pageBuilding2, inputData);
-    docDefenition.content.push({text: 'Weeknummer: ', pageBreak: 'before', alignment: 'center', style: 'subheader'});
-    docDefenition.content.push(table);
-    
-    table = {
-        table: {
-            headerRows: 1,
-            heights: 10,
-            widths: [20, 20, '*', '*', '*', '*', '*', '*', '*', 20, 20, '*', '*', '*', '*', '*', '*', '*'],
-
-            body: []
+                body: []
+            }, layout: {
+                    hLineWidth: function (i, node) {
+                        return (i === 0 || i == 1 || i === node.table.body.length || (i - 1) % page.VentPerAfd == 0) ? 2 : 0.1;
+                    },
+                    vLineWidth: function (i, node) {
+                        return (i === 0 || i == 1 || i == 2 || i == 9 || i == 10 || i == 11 || i === node.table.widths.length) ? 2 : 0.1;
+                    },
+                    hLineColor: function (i, node) {
+                        return 'black';
+                    },
+                    vLineColor: function (i, node) {
+                        return 'black';
+                    },
+                    paddingLeft: function(i, node) { return 5; },
+                    paddingRight: function(i, node) { return 5; },
+                    paddingTop: function(i, node) { return 0.1; },
+                    paddingBottom: function(i, node) { return 0.1; },
+                }
+        };
+        // build the table feurer with input Data
+        buildPage(table, page, inputData);
+        // print the weeknumber on the page if not the first one then with and page
+        if (index == 0) {
+            docDefenition.content.push({ text: 'Weeknummer: ', alignment: 'center', style: 'subheader' });
+        } else {
+            docDefenition.content.push({text: 'Weeknummer: ', pageBreak: 'before', alignment: 'center', style: 'subheader'});
         }
-    };
-    buildPage(table, pageBuilding3, inputData);
-    docDefenition.content.push({text: 'Weeknummer: ', pageBreak: 'before', alignment: 'center', style: 'subheader'});
-    docDefenition.content.push(table);
-
-    table = {
-        table: {
-            headerRows: 1,
-            heights: 10,
-            widths: [20, 20, '*', '*', '*', '*', '*', '*', '*', 20, 20, '*', '*', '*', '*', '*', '*', '*'],
-
-            body: []
-        }
-    };
-    buildPage(table, pageBuilding4, inputData);
-    docDefenition.content.push({text: 'Weeknummer: ', pageBreak: 'before', alignment: 'center', style: 'subheader'});
-    docDefenition.content.push(table);
-
-    table = {
-        table: {
-            headerRows: 1,
-            heights: 10,
-            widths: [20, 20, '*', '*', '*', '*', '*', '*', '*', 20, 20, '*', '*', '*', '*', '*', '*', '*'],
-
-            body: []
-        }
-    };
-    buildPage(table, pageBuilding5, inputData);
-    docDefenition.content.push({text: 'Weeknummer: ', pageBreak: 'before', alignment: 'center', style: 'subheader'});
-    docDefenition.content.push(table);
-
-    table = {
-        table: {
-            headerRows: 1,
-            heights: 10,
-            widths: [20, 20, '*', '*', '*', '*', '*', '*', '*', 20, 20, '*', '*', '*', '*', '*', '*', '*'],
-
-            body: []
-        }
-    };
-    buildPage(table, pageBuilding6, inputData);
-    docDefenition.content.push({text: 'Weeknummer: ', pageBreak: 'before', alignment: 'center', style: 'subheader'});
-    docDefenition.content.push(table);
+        // print table on the page
+        docDefenition.content.push(table);
+    });
 
     console.log(docDefenition);
 
+    // save the document
     var pdfDoc = printer.createPdfKitDocument(docDefenition, option);
     pdfDoc.pipe(fs.createWriteStream('document.pdf'));
     pdfDoc.end();
@@ -266,48 +179,44 @@ function buildPage(targetTable, pageInfo, inputData)
         LTellerVent += pageInfo.VentPerAfd;
         RTellerVent += pageInfo.VentPerAfd;
     }
-    // if (pageInfo.afUp == true) {
-    //     for (var LTellerAfd = pageInfo.LStartAfd, RTellerAfd = pageInfo.RStartAfd; ((pageInfo.LStartAfd + pageInfo.LAantalAfd) > LTellerAfd) || ((pageInfo.RStartAfd + pageInfo.RAantalAfd) > RTellerAfd); LTellerAfd++, RTellerAfd++) {
-    //         buildTable(targetTable, pageInfo, inputData, LTellerAfd, RTellerAfd, LTellerVent, RTellerVent);
-    //         LTellerVent += pageInfo.VentPerAfd;
-    //         RTellerVent += pageInfo.VentPerAfd;
-    //     }
-    // } else {
-    //     for (var LTellerAfd = pageInfo.LStartAfd, RTellerAfd = pageInfo.RStartAfd; ((pageInfo.LStartAfd - pageInfo.LAantalAfd) < LTellerAfd) || ((pageInfo.RStartAfd - pageInfo.RAantalAfd) < RTellerAfd); LTellerAfd--, RTellerAfd--) {
-    //         buildTable(targetTable, pageInfo, inputData, LTellerAfd, RTellerAfd, LTellerVent, RTellerVent);
-    //         LTellerVent += pageInfo.VentPerAfd;
-    //         RTellerVent += pageInfo.VentPerAfd;
-    //     }
-    // }
 }
 
 function buildTable(targetTable, pageInfo, inputData, LTellerAfd, RTellerAfd, LTellerVent, RTellerVent) {
     var LventWaarde, RventWaarde;
-    LventWaarde = searchVentielWaarde(inputData, pageInfo.name, LTellerAfd, LTellerVent);
-    RventWaarde = searchVentielWaarde(inputData, pageInfo.name, RTellerAfd, RTellerVent);
-    targetTable.table.body.push([{text: LTellerAfd, rowSpan: pageInfo.VentPerAfd, alignment: 'center'}, LTellerVent, LventWaarde, '', '', '', '', '', '', {text: RTellerAfd, rowSpan: pageInfo.VentPerAfd, alignment: 'center'}, RTellerVent, RventWaarde, '', '', '', '', '', '']);
-    for (var tellerVent = 1; tellerVent < pageInfo.VentPerAfd - 1; tellerVent++){
-        LTellerVent++;
-        RTellerVent++;
+    if (pageInfo.LafUp ? (LTellerAfd >= pageInfo.LStartAfd + pageInfo.LAantalAfd) : (LTellerAfd <= pageInfo.LStartAfd - pageInfo.LAantalAfd))
+    {
+        RventWaarde = searchVentielWaarde(inputData, pageInfo.name, RTellerAfd, RTellerVent);
+        targetTable.table.body.push(['', '', '', '', '', '', '', '', '', {text: RTellerAfd, rowSpan: pageInfo.VentPerAfd, alignment: 'center'}, RTellerVent, RventWaarde, '', '', '', '', '', '']);
+    } else if (pageInfo.RafUp ? (RTellerAfd >= pageInfo.RStartAfd + pageInfo.RAantalAfd) : (RTellerAfd <= pageInfo.RStartAfd - pageInfo.RAantalAfd)) {
+        LventWaarde = searchVentielWaarde(inputData, pageInfo.name, LTellerAfd, LTellerVent);
+        targetTable.table.body.push([{text: LTellerAfd, rowSpan: pageInfo.VentPerAfd, alignment: 'center'}, LTellerVent, LventWaarde, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']);
+    } else {
         LventWaarde = searchVentielWaarde(inputData, pageInfo.name, LTellerAfd, LTellerVent);
         RventWaarde = searchVentielWaarde(inputData, pageInfo.name, RTellerAfd, RTellerVent);
-        targetTable.table.body.push(['', LTellerVent, LventWaarde, '', '', '', '', '', '', '', RTellerVent, RventWaarde, '', '', '', '', '', '']);
+        targetTable.table.body.push([{text: LTellerAfd, rowSpan: pageInfo.VentPerAfd, alignment: 'center'}, LTellerVent, LventWaarde, '', '', '', '', '', '', {text: RTellerAfd, rowSpan: pageInfo.VentPerAfd, alignment: 'center'}, RTellerVent, RventWaarde, '', '', '', '', '', '']);
     }
-    LTellerVent++;
-    RTellerVent++;
-    LventWaarde = searchVentielWaarde(inputData, pageInfo.name, LTellerAfd, LTellerVent);
-    RventWaarde = searchVentielWaarde(inputData, pageInfo.name, RTellerAfd, RTellerVent);
-    targetTable.table.body.push(['', LTellerVent, LventWaarde, '', '', '', '', '', '', '', RTellerVent, RventWaarde, '', '', '', '', '', '']);
+    for (var tellerVent = 1; tellerVent < pageInfo.VentPerAfd; tellerVent++){
+        LTellerVent++;
+        RTellerVent++;
+        if (pageInfo.LafUp ? (LTellerAfd >= pageInfo.LStartAfd + pageInfo.LAantalAfd) : (LTellerAfd <= pageInfo.LStartAfd - pageInfo.LAantalAfd))
+        {
+            RventWaarde = searchVentielWaarde(inputData, pageInfo.name, RTellerAfd, RTellerVent);
+            targetTable.table.body.push(['', '', '', '', '', '', '', '', '', '', RTellerVent, RventWaarde, '', '', '', '', '', '']);
+        } else if (pageInfo.RafUp ? (RTellerAfd >= pageInfo.RStartAfd + pageInfo.RAantalAfd) : (RTellerAfd <= pageInfo.RStartAfd - pageInfo.RAantalAfd)) {
+            LventWaarde = searchVentielWaarde(inputData, pageInfo.name, LTellerAfd, LTellerVent);
+            targetTable.table.body.push(['', LTellerVent, LventWaarde, '', '', '', '', '', '', '', '', '', '', '', '', '', '', '']);
+        } else {
+            LventWaarde = searchVentielWaarde(inputData, pageInfo.name, LTellerAfd, LTellerVent);
+            RventWaarde = searchVentielWaarde(inputData, pageInfo.name, RTellerAfd, RTellerVent);
+            targetTable.table.body.push(['', LTellerVent, LventWaarde, '', '', '', '', '', '', '', RTellerVent, RventWaarde, '', '', '', '', '', '']);
+        }
+    }
     LTellerVent++;
     RTellerVent++;
 }
 
 function searchVentielWaarde(inputData, stal, afdeling, ventiel) {
     var ventielWaarde = '';
-
-    //var result = jsonQuery("Locatie=' " + stal + "  -  Afdeling " + afdeling.toString() + "  -  Ventiel " + ventiel.toString() + "'", { data: inputData }).value;
-    // var result = jsonQuery("[Aantal=26]", { data: inputData }).value;
-    // console.log(result);
 
     for (var vent in inputData) {
         if (inputData[vent].Locatie == ' ' + stal +'  -  Afdeling ' + afdeling + '  -  Ventiel ' + ventiel) {
