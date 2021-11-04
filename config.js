5cd3e2f6ce9077f86b0fbe4f4591a4770683652e
BruneelBen <benbruneel@hotmail.com>
2021-11-04 22:15:01 +0100

    start making config file

---
+++ config.js
@@ -0,0 +1,15 @@
+
+// required librys
+const path = require("path");
+const fs = require("fs");
+
+function read() {
+
+}
+
+function write() {
+    
+}
+
+// export the function for other functions tu use
+module.exports = { read, write };
