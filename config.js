2ed8c8c13c0aeddec0a3f1875ac96c8cf141b8fb
BruneelBen <benbruneel@hotmail.com>
2021-11-06 17:43:31 +0100

    Make all needed functions for read, write and change parameters for in config file

--- config.js
+++ config.js
@@ -3,13 +3,45 @@
 const path = require("path");
 const fs = require("fs");
 
+// read config file
 function read() {
 
 }
 
+// save config file
 function write() {
     
 }
 
+// give all element IDs for make a overview
+function getList() {
+
+}
+
+// give the numbers of rows that there are
+function Count() {
+
+}
+
+// get the data of a element
+function getElement(elementId) {
+
+}
+
+// update the data of a element
+function setElement(elmentId, jsonString) {
+
+}
+
+// delete a element
+function removeElement(elementId) {
+
+}
+
+// add a new element
+function addElement(elmentIc, jsonString) {
+
+}
+
 // export the function for other functions tu use
-module.exports = { read, write };
+module.exports = { read, write, getList, Count, getElement, setElement, removeElement, addElement };
